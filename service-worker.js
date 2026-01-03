7419a7778e913d3291d637b698cfa8cef33b6a17
Joshua Sangmeister <joshua.sangmeister@gmail.com>
2026-01-03 16:59:36 +0100

    Debugging

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,3 @@
-// Install and activate events
 self.addEventListener('install', event => {
   event.waitUntil(self.skipWaiting());
 });
@@ -8,17 +7,5 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-  if (event.request.method === 'POST') {
-    event.respondWith((async () => {
-      const formData = await event.request.formData();
-      const sharedUrl = formData.get('url');
-      const url = new URL(sharedUrl);
-      url.search = ''; // Query entfernen
-      const target = `https://archive.ph/newest/${url.toString()}`;
-      return Response.redirect(`index.html?url=${encodeURIComponent(target)}`, 303);
-    })());
-    return;
-  }
   event.respondWith(fetch(event.request));
 });
